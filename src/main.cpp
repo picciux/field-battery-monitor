aa1069dc371eda2d8f34b4fb2c4d16c3fce57393
Matteo Piscitelli <matteo@matteopiscitelli.it>
2026-09-16 18:12:16 +0200

    Call sumbmodules "run" code at max speed.

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,7 @@ void setup() {
 }
 
 void loop() {
-  // 1. Mantiene vivi i servizi di rete, WebSocket e richieste HTTP del tuo codice
   wifi.run();
   BTHomeBeacon_run(hardware);
-  // 2. Timer non bloccante per leggere i sensori ed emettere il beacon BLE ogni 5 secondi
-  static unsigned long last_bms_time = 0;
-  if (millis() - last_bms_time >= 1000) {
-    last_bms_time = millis();
-    hardware.run();
-  }
+  hardware.run();
 }
