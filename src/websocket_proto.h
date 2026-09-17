86d526abc390c279ff02baa232c1d41b15e1a37c
Matteo Piscitelli <matteo@matteopiscitelli.it>
2026-09-17 18:56:18 +0200

    Define websocket protocol strings.

---
+++ src/websocket_proto.h
@@ -0,0 +1,72 @@
+
+/* resets SoC to 100% */
+#define ACTION_BATTERY_SOC_RESET        "battery_soc_reset"
+
+/* sets cold protection low threshold temperature. 
+   Pars:
+    - float temperature 
+*/
+#define ACTION_CP_SET_LT                "cp_low_threshold"
+
+/* manually sets light brightness.
+   Pars:
+    - float brightness (0.0 - 1.0)
+*/
+#define ACTION_LIGHT_BRIGHTNESS         "light_brightness"
+
+/* sets light motion automation.
+   Pars:
+    - bool enabled
+*/
+#define ACTION_LIGHT_AUTO_ENABLE        "light_auto_enabled"
+
+/* sets light automation turn-on brightness.
+   Pars:
+    - float brightness (0.0 - 1.0)
+*/
+#define ACTION_LIGHT_AUTO_BRIGHTNESS    "light_auto_brightness"
+
+/* sets light automation after-motion duration
+   Pars:
+    - int seconds
+*/
+#define ACTION_LIGHT_AUTO_DURATION      "light_auto_duration"
+
+/* sets power outlet
+   Pars:
+    - int outlet index
+    - float power (0.0 - 1.0)
+*/
+#define ACTION_OUTLET_POWER             "outlet_power"
+
+/* update battery state event.
+   Pars:
+    - float voltage
+    - float current
+    - float SoC
+    - float temperature 
+*/
+#define EVENT_BATTERY   "battery_update"
+
+/* update cold protection state event.
+   Pars:
+    - bool enabled/disabled
+    - int low threshold temperature 
+*/
+#define EVENT_CP        "cold_protection_update"
+
+/* update light state event.
+   Pars: 
+    - float light brightness
+    - bool auto enabled/disabled
+    - float auto brightness
+    - int auto duration 
+*/
+#define EVENT_LIGHT     "light update"
+
+/* update power outlets state event.
+   Pars:
+    - int outlet index
+    - float power 
+*/
+#define EVENT_OUTLETS   "outlets_update"
